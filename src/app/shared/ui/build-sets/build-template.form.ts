8941d25cb69e81833f8ad38b4b47f66c1eea2789
Josip Mihelko <josip.mihelko@gmail.com>
2022-11-26 12:15:47 +0100

    Edit Build Set Templates.

--- src/app/shared/ui/build-sets/build-template.form.ts
+++ src/app/shared/ui/build-sets/build-template.form.ts
@@ -1,7 +1,7 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {IBranch, IBuildTemplate, IProject} from "../../../core";
+import {IBuildTemplate} from "../../../core";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
 import {MatCardModule} from "@angular/material/card";
